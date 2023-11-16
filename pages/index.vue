36b0bbf9c90bb4cb0342579297f0c4d24af2338c
HarukiOkamoto <alice.159357@icloud.com>
2023-11-17 01:08:56 +0900

    11/17

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@ const clickDay = (day: string) => {
 
     <div class="calendar-container">
         <table class="calendar">
-            <tr class="week-one">
+            <tr class="week">
                 <td :class="{ 'clicked-class': isClicked('day0') }" @click="clickDay('day0')">
                     <div class="circle"></div>
                     0
@@ -64,7 +64,109 @@ const clickDay = (day: string) => {
                     9
                 </td>
             </tr>
+
+
+            <tr class="week">
+                <td :class="{ 'clicked-class': isClicked('day10') }" @click="clickDay('day10')">
+                    <div class="circle"></div>
+                    10
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day11') }" @click="clickDay('day11')">
+                    <div class="circle"></div>
+                    11
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day12') }" @click="clickDay('day12')">
+                    <div class="circle"></div>
+                    12
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day13') }" @click="clickDay('day13')">
+                    <div class="circle"></div>
+                    13
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day14') }" @click="clickDay('day14')">
+                    <div class="circle"></div>
+                    14
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day15') }" @click="clickDay('day15')">
+                    <div class="circle"></div>
+                    15
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day16') }" @click="clickDay('day16')">
+                    <div class="circle"></div>
+                    16
+                </td>
+            </tr>
+
+            <tr class="week">
+                <td :class="{ 'clicked-class': isClicked('day17') }" @click="clickDay('day17')">
+                    <div class="circle"></div>
+                    17
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day18') }" @click="clickDay('day18')">
+                    <div class="circle"></div>
+                    18
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day19') }" @click="clickDay('day19')">
+                    <div class="circle"></div>
+                    19
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day20') }" @click="clickDay('day20')">
+                    <div class="circle"></div>
+                    20
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day21') }" @click="clickDay('day21')">
+                    <div class="circle"></div>
+                    21
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day22') }" @click="clickDay('day22')">
+                    <div class="circle"></div>
+                    22
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day23') }" @click="clickDay('day23')">
+                    <div class="circle"></div>
+                    23
+                </td>
+            </tr>
+
+            <tr class="week">
+                <td :class="{ 'clicked-class': isClicked('day24') }" @click="clickDay('day24')">
+                    <div class="circle"></div>
+                    24
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day25') }" @click="clickDay('day25')">
+                    <div class="circle"></div>
+                    25
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day26') }" @click="clickDay('day26')">
+                    <div class="circle"></div>
+                    26
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day27') }" @click="clickDay('day27')">
+                    <div class="circle"></div>
+                    27
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day28') }" @click="clickDay('day28')">
+                    <div class="circle"></div>
+                    28
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day29') }" @click="clickDay('day29')">
+                    <div class="circle"></div>
+                    29
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day30') }" @click="clickDay('day30')">
+                    <div class="circle"></div>
+                    30
+                </td>
+                <td :class="{ 'clicked-class': isClicked('day31') }" @click="clickDay('day31')">
+                    <div class="circle"></div>
+                    31
+                </td>
+            </tr>
         </table>
+
+        <img v-if="isClicked('day0')" src="@/assets/img/1-day0.jpg">
+
+
     </div>
 </template>
 
@@ -76,18 +178,19 @@ const clickDay = (day: string) => {
 }
 
 /* 孫 */
-.week-one {
+.week {
     width: 50%;
     height: auto;
 }
 
-.week-one td {
+.week td {
     text-align: center;
     position: relative;
+    cursor: pointer;
 }
 
 /* 透明な円 */
-.week-one .circle {
+.week .circle {
     position: absolute;
     top: 50%;
     left: 50%;
@@ -109,7 +212,7 @@ const clickDay = (day: string) => {
 
 /* メディアクエリ - スマートフォン向け */
 @media screen and (max-width: 767px) {
-    .week-one {
+    .week {
         width: 80%;
         /* 幅を80%に調整 */
     }
@@ -117,7 +220,7 @@ const clickDay = (day: string) => {
 
 /* メディアクエリ - タブレット向け */
 @media screen and (min-width: 768px) and (max-width: 1024px) {
-    .week-one {
+    .week {
         width: 60%;
         /* 幅を60%に調整 */
     }
@@ -125,7 +228,7 @@ const clickDay = (day: string) => {
 
 /* メディアクエリ - デスクトップ向け */
 @media screen and (min-width: 1025px) {
-    .week-one {
+    .week {
         width: 40%;
         /* 幅を50%に調整 */
     }
